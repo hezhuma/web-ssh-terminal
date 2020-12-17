95e794483ac9e51da0d72b39508b01ec48902af6
贺释贤 <he_shx@hisuntech.com>
2020-12-17 11:02:17 +0800

    init project

---
+++ src/test/java/com/hezhuma/WebSshTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hezhuma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSshTerminalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
